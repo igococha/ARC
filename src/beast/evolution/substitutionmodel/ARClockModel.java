c9c7fc9b4ecceb3853984d0862dbd34f5aa955d4
Igor Siveroni <igor@siveroni.com>
2019-09-25 16:49:18 +0100

    first commit

---
+++ src/beast/evolution/substitutionmodel/ARClockModel.java
@@ -0,0 +1,24 @@
+package beast.evolution.substitutionmodel;
+
+import beast.evolution.branchratemodel.BranchRateModel.Base;
+import beast.evolution.tree.Node;
+
+public class ARClockModel extends Base {
+
+	public ARClockModel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double getRateForBranch(Node node) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void initAndValidate() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
