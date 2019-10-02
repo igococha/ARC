2bee2dbbcb387de729c6dc7a73c8c86e3932e673
Igor Siveroni <igor@siveroni.com>
2019-10-02 19:33:07 +0100

    first implementation

--- src/beast/evolution/substitutionmodel/ARClockModelTemplate.java
+++ src/beast/evolution/substitutionmodel/ARClockModelTemplate.java
@@ -3,7 +3,6 @@ package beast.evolution.substitutionmodel;
 import java.util.Arrays;
 
 import org.apache.commons.math.MathException;
-import org.apache.commons.math.distribution.ContinuousDistribution;
 import org.apache.commons.math.distribution.GammaDistribution;
 import org.apache.commons.math.distribution.GammaDistributionImpl;
 
@@ -24,13 +23,13 @@ import beast.util.Randomizer;
  * @author Igor Siveroni
  */
 
-@Description("Implements ARC: th Additive Uncorrelated Ralezed Clock Model")
+@Description("Implements ARC: Additive Uncorrelated Ralezed Clock Model")
 @Citation(value =
         "Didelot X and Volz EM (2019) Additive uncorrelated  relaxed clock models\n" +
                 "  for the dating of epidemiological models.", DOI = "TBC",
         year = 2019, firstAuthorSurname = "Didelot")
 
-public class ARClockModel extends Base {
+public class ARClockModelTemplate extends Base {
 	final public Input<ParametricDistribution> rateDistInput = new Input<>("distr", "the distribution governing the rates among branches. Must have mean of 1. The clock.rate parameter can be used to change the mean rate.", Input.Validate.REQUIRED);
     final public Input<IntegerParameter> categoryInput = new Input<>("rateCategories", "the rate categories associated with nodes in the tree for sampling of individual rates among branches.", Input.Validate.REQUIRED);
     final public Input<Integer> numberOfDiscreteRates = new Input<>("numberOfDiscreteRates", "the number of discrete rate categories to approximate the rate distribution by. A value <= 0 will cause the number of categories to be set equal to the number of branches in the tree. (default = -1)", -1);
@@ -38,49 +37,49 @@ public class ARClockModel extends Base {
     final public Input<Tree> treeInput = new Input<>("tree", "the tree this relaxed clock is associated with.", Input.Validate.REQUIRED);
     final public Input<Boolean> normalizeInput = new Input<>("normalize", "Whether to normalize the average rate (default false).", false);
    
-    // Gamma distribution parameters
-    final public Input<RealParameter> ratesMeanInput = new Input<>("rateMean", "mean of rates Gamma probability distribution", Input.Validate.REQUIRED);
-    final public Input<RealParameter> ratesOmegaInput = new Input<>("rateOmega", "omega factor of rates Gamma distribution", Input.Validate.REQUIRED);
-
 	
     boolean useCategories;
     // either categories or rateProbsParameter is used
     RealParameter rateProbs; //when mode=rates
     IntegerParameter categories; //when mode=categories
     
+    ParametricDistribution distribution; //the distribution of the rates
+    // i.e. LogNormal(M,S,MeanInRealSpace), then get the three parameters
+    double M;double S;boolean MeanInRealSpace;
     
-    
-    ContinuousDistribution gammaDist;
-    RealParameter ratesMean = ratesMeanInput.get();
-    RealParameter ratesOmega = ratesOmegaInput.get();
+    GammaDistribution m_dist = new GammaDistributionImpl(1, 1);
 
     RealParameter meanRate;
     Tree tree;
     private int branchCount;//the number of branches of the tree
-    private int nodeCount;
-    private boolean normalize = false;
-    
-    int numCategories = 100;
-    
-    private boolean recomputeScaleFactor = true;
+    private boolean normalize = false;//   
+    private boolean renormalize = true;
     
     private boolean recompute = true;
-    
-    // state that we might want to store/restore
-    
-    double[] unscaledBranchRates;
-    double[] storedUnscaledBranchRates;
-    private double scaleFactor = 1.0; 
-    private double storedScaleFactor = 1.0;
-    
+    private double[] ratesxCategory; //the output rates
+    private double[] storedRatesxCategory; //
+    private double scaleFactor = 1.0; //initial
+    private double storedScaleFactor = 1.0; //initial
+    int numCategories = 100;//
+
 
     @Override
 	public void initAndValidate() {
 		tree = treeInput.get();
-		nodeCount = tree.getNodeCount();
-		branchCount = nodeCount - 1;
+		branchCount = tree.getNodeCount() - 1;
 		categories = categoryInput.get();
 		rateProbs = rateProbsInput.get();
+		distribution = rateDistInput.get();
+		
+		//get the mean and standard deviation of lognormal distribution
+        if(distribution instanceof LogNormalDistributionModel){
+            LogNormalDistributionModel mylognormal=(LogNormalDistributionModel)distribution;
+             M=mylognormal.MParameterInput.get().getValue();
+             S=mylognormal.SParameterInput.get().getValue();
+             MeanInRealSpace=mylognormal.hasMeanInRealSpaceInput.get();
+        } else {
+        	throw new IllegalArgumentException("ARClock model only implemented for LogNormal distribution");
+        }
         
         if (categories==null) useCategories=true;
         else useCategories = true;
@@ -88,13 +87,13 @@ public class ARClockModel extends Base {
         if (useCategories) {
             numCategories = numberOfDiscreteRates.get();
             if (numCategories <= 0) numCategories = branchCount;
-            Log.info.println("  ARClockModel: using " + numCategories + " rate " +
+            Log.info.println("  ARCClockModel: using " + numCategories + " rate " +
                     "categories to approximate rate distribution across branches.");
         } else {
             if (numberOfDiscreteRates.get() != -1) {  // if it's not the default i.e. it has been set as input
                 throw new IllegalArgumentException("Can't specify both numberOfDiscreteRates and  rates inputs.");
             }
-            Log.info.println("  ARClockModel: using rate probs as parameter for sampling across branches.");          
+            Log.info.println("  ARClockModel: using rate probs as parameter for sampling  across branches.");          
         }
         
         //initialize rates in both types of inputs
@@ -124,27 +123,47 @@ public class ARClockModel extends Base {
         	rateProbs.setUpper(1.0);
         }
         
-       
+        if (useCategories) {
+            // rates are initially zero and are computed by getRawRate(int i) as needed
+            ratesxCategory = new double[numCategories];
+            storedRatesxCategory = new double[numCategories];
+        }
         normalize = normalizeInput.get();
-        
-        meanRate = meanRateInput.get();  // from superclass
+        meanRate = meanRateInput.get();
         if (meanRate == null) {
             meanRate = new RealParameter("1.0");
         }
-        
-        if (Math.abs(ratesMean.getValue()-1.0) > 1e-6 ) { // igor: should this be ONE in ARC's case?
+        if (Math.abs(distribution.getMean()-1.0) > 1e-6 ) { // igor: should this be ONE in ARC's case?
         	Log.warning.println("WARNING: mean of distribution for additive relaxed clock model is not 1.0.");
         }
        
-        // new
-        unscaledBranchRates = new double[tree.getNodeCount()];
+        m_dist = new GammaDistributionImpl(1, 1);
+
 		
 	}
 	
     private void initialize() {
-        // code to be executed whenever we start a fresh MCMC run (and no restore has been called)
+        if (useCategories) {
+           Arrays.fill(ratesxCategory, 0.0);
+        }
     }
 	
+	/**
+     * Computes a scale factor for normalization. Only called if normalize=true.
+     */
+    private void computeScaleFactor() {
+        //scale mean rate to 1.0 or separate parameter
+        double treeRate = 0.0;
+        double treeTime = 0.0;
+        for (int i = 0; i < tree.getNodeCount(); i++) {
+            Node node = tree.getNode(i);
+            if (!node.isRoot()) {
+                treeRate += getRawRate(node) * node.getLength();
+                treeTime += node.getLength();
+            }
+        }
+        scaleFactor = 1.0 / (treeRate / treeTime);
+    }
 
 	//get the rate for node: R = r*scale*meanRate
 	@Override
@@ -152,134 +171,114 @@ public class ARClockModel extends Base {
 		if (node.isRoot()) {
             return 1; // root has no rate
         }
-		synchronized (this) {
-    		if (recompute) {
-    			calculateUnscaledBranchRates();
-                recalculateScaleFactor();
+        if (recompute) {
+           // avoid thread clash
+            synchronized (this) {
+                initialize();
                 recompute = false;
-			}
+            }
         }
-
-        return unscaledBranchRates[getNr(node)] * scaleFactor;	
-     
-	}
-	
-	private void recalculateScaleFactor() {
-        if (normalize) {
-            double timeTotal = 0.0;
-            double branchTotal = 0.0;
-
-            for (int i = 0; i < tree.getNodeCount(); i++) {
-                Node node = tree.getNode(i);
-                if (!node.isRoot()) {
-                    double branchInTime = node.getLength();
-                    double branchLength = branchInTime * unscaledBranchRates[node.getNr()];
-                    timeTotal += branchInTime;
-                    branchTotal += branchLength;
+        if (renormalize) {
+            if (normalize) {
+                synchronized (this) {
+                    computeScaleFactor();
                 }
             }
-
-            scaleFactor = timeTotal / branchTotal;
-
-            scaleFactor *= meanRate.getValue();
-        } else {
-            scaleFactor = 1.0;
+            renormalize = false;
         }
-        // scaleFactor *= meanRate.getValue();
-    }
-	
-    
-	private void calculateUnscaledBranchRates() {
-		if (this.useCategories)
-			calculateUnscaledRatesForCategories();
-		else
-			calculateUnscaledRatesForRates();
+        return getRawRate(node) * scaleFactor * meanRate.getValue();
 	}
-    
-    
-    
-    private void calculateUnscaledRatesForCategories() {
-    	double rate=1.0;
-    	
-    	for(int i=0; i < tree.getNodeCount();i++) {
-    		final Node node = tree.getNode(i);
-    		final int nr = getNr(node);
-    		if (! node.isRoot()) {   		
-    			int category = categories.getValue(nr);
-    			final double beta = node.getLength() / ratesOmega.getValue();
-    			final double alpha = ratesMean.getValue() * beta;
-    			gammaDist = new GammaDistributionImpl(alpha, beta); 
-    			try {
-    				rate = gammaDist.inverseCumulativeProbability( (category + 0.5) / numCategories );
-    			} catch (MathException e) {
-    				throw new RuntimeException("Failed to compute inverse cumulative probability");
-    			}
-    		}
-    		unscaledBranchRates[nr] = rate;
-    	}   	
-    }
-    
-    private void calculateUnscaledRatesForRates() {
-    	double rate=1.0;
-    	
-    	for(int i=0; i < tree.getNodeCount();i++) {
-    		final Node node = tree.getNode(i);
-    		final int nr = getNr(node);
-    		if (! node.isRoot()) {   		
-    			double p = rateProbs.getValue(nr);
-    	        final double beta = node.getLength() / ratesOmega.getValue();
-    	        final double alpha = ratesMean.getValue() * beta;
-    	        gammaDist = new GammaDistributionImpl(alpha, beta); 
-    	        try {
-    	        	rate = gammaDist.inverseCumulativeProbability( p );
-    	        } catch (MathException e) {
-    	        	throw new RuntimeException("Failed to compute inverse cumulative probability");
-    	        }
-    		}
-    		unscaledBranchRates[nr] = rate;
-    	}   	
-    }
-    
-    
 	
-    /*  computes the idx used to access the arrays a[branchCount] 
-     * needed in case of node swaps involving the root */
-    private int getNr(Node node) {
-        int nodeNr = node.getNr();
-        if (nodeNr > tree.getRoot().getNr()) {
-            nodeNr--;
-        }
-        return nodeNr;
-    }
-
-    // indexing used by UCRelaxedClock
-    private int getNr2(Node node) {
-    	int nodeNr = node.getNr();
-        if (nodeNr == branchCount) { // this should be root if no swap has happened
-            // root node has nr less than #categories, so use that nr
-            nodeNr = node.getTree().getRoot().getNr();
-        }
-        return nodeNr;
+    private double getRawRate(Node node) {
+    	if (useCategories) {
+    		return getRawRateForCategories(node);
+    	} else {
+    		return getRawRateForRates(node);
+    	}
     }
     
-    // Come back when parameters have settled - so far, recompute always
+    private double getRawRateForCategories(Node node) {
+        int nodeNumber = node.getNr();
+        if (nodeNumber == branchCount) {
+            // root node has nr less than #categories, so use that nr
+            nodeNumber = node.getTree().getRoot().getNr();
+        }
+        int category = categories.getValue(nodeNumber);
+        if (ratesxCategory[category] == 0.0) {
+            try {
+                ratesxCategory[category] = distribution.inverseCumulativeProbability((category + 0.5) / numCategories);
+            } catch (MathException e) {
+                throw new RuntimeException("Failed to compute inverse cumulative probability");
+            }
+        }
+        return ratesxCategory[category];
+    }
+    
+    private double getRawRateForRates(Node node) {
+    	int nodeNumber = node.getNr();
+        if (nodeNumber == branchCount) {
+            // root node has nr less than #categories, so use that nr
+            nodeNumber = node.getTree().getRoot().getNr();
+        }
+        double p = rateProbs.getValue(nodeNumber);
+        double rate;
+        try {
+            rate = distribution.inverseCumulativeProbability(p);
+        } catch (MathException e) {
+            throw new RuntimeException("Failed to compute inverse cumulative probability");
+        }
+    	return rate;
+    }
+	
+    
+    // Need to review this
     @Override
     protected boolean requiresRecalculation() {
-        recompute = true;
-        recomputeScaleFactor = true;
+        recompute = false;
+        renormalize = true;
+
+//        if (treeInput.get().somethingIsDirty()) {
+//        	recompute = true;
+//            return true;
+//        }
+        
+        // rateDistInput cannot be dirty?!?
+        if (rateDistInput.get().isDirtyCalculation()) {
+            recompute = true;
+            return true;
+        }
+        // NOT processed as trait on the tree, so DO mark as dirty
+        if (categoryInput.get() != null && categoryInput.get().somethingIsDirty()) {
+            //recompute = true;
+            return true;
+        }
+
+       
+        if (meanRate.somethingIsDirty()) {
+            return true;
+        }
 
         return recompute;
     }
     
     @Override
     public void store() {
-    	recompute = true;
+    	if (useCategories) {
+           System.arraycopy(ratesxCategory, 0, storedRatesxCategory, 0, numCategories);
+            
+        }
+        storedScaleFactor = scaleFactor;
         super.store();
     }
 
     @Override
     public void restore() {
-    	recompute = true;
+        if(useCategories){
+            double[] tmp = ratesxCategory;
+            ratesxCategory = storedRatesxCategory;
+            storedRatesxCategory = tmp;
+        }
+        scaleFactor = storedScaleFactor;
         super.restore();
     }
    
